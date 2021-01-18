88daeb51b96175487400c4b005189d690b3a84da
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-01-18 08:50:47 -0500

    styled btns and positioning on top of screen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,22 +1,49 @@
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { View, SafeAreaView, Image, StyleSheet } from "react-native";
 
 function ViewImageScreen(props) {
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteView}></View>
+
       <Image
         style={styles.image}
+        resizeMode="contain"
         source={require("../assets/chair.jpg")}
-      ></Image>
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
   image: {
     width: "100%",
     height: "100%",
-    resizeMode: "contain",
+  },
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteView: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ECDC4",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
 
