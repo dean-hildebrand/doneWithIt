038dbccc17e3f388bee4f8b8f49e4977427c80ce
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-01-18 10:12:00 -0500

    styled button component and onpress handler is working to log press

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import AppButton from "./app/components/AppButton";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
+// import colors from "./config/colors";
+
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>
+        <AppButton
+          title="Login"
+          onPress={() => console.log("pressing button")}
+        />
+        ;
+      </Text>
+    </View>
+  );
 }
