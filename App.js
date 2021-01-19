aa92c41680b435301f924f3f37d407302acb86b7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-01-19 09:17:54 -0500

    Card component and hard coded image and titles

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Text, View } from "react-native";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 // import colors from "./config/colors";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#DFDBDB",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
