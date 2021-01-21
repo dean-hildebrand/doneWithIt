54f677d92656cb4512439295a030635e4fc563de
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-01-21 09:36:39 -0500

    FlatList component to render list items

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+
+const messages = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "Description 1",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    description: "Description 2",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  return (
+    <FlatList
+      data={messages}
+      // keyExtractor: pass function used to extract unit key from each object in array (unique indentifier)
+      keyExtractor={(message) => message.id.toString()}
+      //using list item component
+      renderItem={({ item }) => (
+        <ListItem
+          title={item.title}
+          subTitle={item.subTitle}
+          image={item.image}
+        />
+      )}
+    />
+  );
+}
+
+export default MessagesScreen;
