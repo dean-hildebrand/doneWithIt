a77ce9c836c5e8fba40ec2a4a2930069ab17ac07
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-02-17 10:16:48 -0500

    Stack navigator practice to switch screens

--- App.js
+++ App.js
@@ -1,31 +1,60 @@
-import React, { useState, useEffect } from "react";
-import * as ImagePicker from "expo-image-picker";
-// import * as Permissions from "expo-permissions";
+import React from "react";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { Button, Image, Text, TextInput, View } from "react-native";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import MyAccountScreen from "./app/screens/MyAccountScreen";
-import Icon from "./app/components/Icon";
 import Screen from "./app/components/Screen";
-import ListItem from "./app/components/lists/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import { Switch } from "react-native";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import CategoryPickerItem from "./app/components/CategoryPickerItem";
 
-// import colors from "./config/colors";
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
+
+// import * as ImagePicker from "expo-image-picker";
+// // import * as Permissions from "expo-permissions";
+
+// import { Button, Image, Text, TextInput, View } from "react-native";
+// import AppButton from "./app/components/AppButton";
+// import Card from "./app/components/Card";
+// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+// import MessagesScreen from "./app/screens/MessagesScreen";
+// import ViewImageScreen from "./app/screens/ViewImageScreen";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+// import MyAccountScreen from "./app/screens/MyAccountScreen";
+// import Icon from "./app/components/Icon";
+// import Screen from "./app/components/Screen";
+// import ListItem from "./app/components/lists/ListItem";
+// import AccountScreen from "./app/screens/AccountScreen";
+// import ListingsScreen from "./app/screens/ListingsScreen";
+// import { Switch } from "react-native";
+// import AppPicker from "./app/components/AppPicker";
+// import LoginScreen from "./app/screens/LoginScreen";
+// import RegisterScreen from "./app/screens/RegisterScreen";
+// import ListingEditScreen from "./app/screens/ListingEditScreen";
+// import ImageInput from "./app/components/ImageInput";
+// import ImageInputList from "./app/components/ImageInputList";
+// import CategoryPickerItem from "./app/components/CategoryPickerItem";
+
+// import colors from "./config/colors";
