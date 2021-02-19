39a6d4bd8f41a336d9c84ff5a3f5ca7f063fc3f7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-02-19 09:51:59 -0500

    Bottom tabs test

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
 
-const Tweets = () => (
+const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
   </Screen>
 );
-const TweetDetails = () => (
+const TweetDetails = ({ route }) => (
   <Screen>
     <Text>Tweet Details</Text>
+    <Text>{route.params.id}</Text>
   </Screen>
 );
 
@@ -20,14 +26,32 @@ const Stack = createStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen
+      options={({ route }) => ({ title: route.params.id })}
+      name="TweetDetails"
+      component={TweetDetails}
+    />
   </Stack.Navigator>
+);
+
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={Tweets} />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
 );
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
