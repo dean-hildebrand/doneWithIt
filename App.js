efea584d54bcff2b5caacc1439207745f755809b
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-02-23 10:04:59 -0500

    created post method for new listing using newFormData

--- App.js
+++ App.js
@@ -5,29 +5,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
 // import * as Permissions from "expo-permissions";
 
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import MyAccountScreen from "./app/screens/MyAccountScreen";
-import Icon from "./app/components/Icon";
-import Screen from "./app/components/Screen";
-import ListItem from "./app/components/lists/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import CategoryPickerItem from "./app/components/CategoryPickerItem";
 import navigationTheme from "./app/navigation/navigationTheme";
-
 import AppNavigator from "./app/navigation/AppNavigator";
-import AccountNavigator from "./app/navigation/AccountNavigator";
 
 // import colors from "./config/colors";
 
